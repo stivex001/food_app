bed9e9df511cbb5ab85d867bae6ef99cb33303e6
stivex001 <stephenadeyemo@gmail.com>
2023-01-13 02:09:36 +0100

    feat: used fetch to get data from firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,32 @@
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
+import { useEffect, useState } from 'react';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([])
+
+  useEffect(() => {
+    const fecthPastries = async () => {
+     const response = await fetch('https://bai-food-app-default-rtdb.firebaseio.com/meals.json')
+     const responseData = await response.json()
+
+     const loadedPastries = []
+     for (const key in responseData) {
+      loadedPastries.push({
+        id: key,
+        name: responseData[key].name,
+        description: responseData[key].description,
+        price: responseData[key].price
+      })
+     }
+     setMeals(loadedPastries)
+    }  
+    fecthPastries()
+  }, [])
+  
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
