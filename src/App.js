cd6dbf8f4f886501eab99c62183254c73b575396
stivex001 <stephenadeyemo@gmail.com>
2023-01-13 01:40:20 +0100

    refactored the code base

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import { useState } from "react";
-import Cart from "./components/Cart/Cart";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import CardProvider from "./store/CardProvider";
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
     setCartIsShown(true);
-  }
+  };
 
   const hideCartHandler = () => {
     setCartIsShown(false);
-  }
+  };
 
   return (
-    <CardProvider>
-      {cartIsShown && <Cart onCloseCart={hideCartHandler} />}
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </CardProvider>
+    </CartProvider>
   );
 }
 
--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const createContext = React.createContext({
-    items: [],
-    totalAmount: 0,
-    addItem: (item) => {},
-    removeItem: (item) => {},
-})
+const CartContext = React.createContext({
+  items: [],
+  totalAmount: 0,
+  addItem: (item) => {},
+  removeItem: (id) => {}
+});
 
-export default createContext;
+export default CartContext;
